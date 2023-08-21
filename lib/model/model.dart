b2a30f24f4c52e11cfca24a5f97c9fc09e1b5567
USER <lubnalubz5714@gmail.com>
2023-08-21 20:27:39 +0530

    api

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,25 +1,45 @@
+// class User{
+//   int? userId;
+//   int? id;
+//   String? title;
+//   String? body;
+//
+//   User({this.userId, this.id, this.title, this.body});
+//
+//
+//   User.fromJson(Map<String, dynamic> json) {
+//     userId = json['userId'];
+//     id = json['id'];
+//     title = json['title'];
+//     body = json['body'];
+//   }
+//   Map<String, dynamic> toJson() {
+//     final Map<String, dynamic> data = new Map<String, dynamic>();
+//     data['userId'] = this.userId;
+//     data['id'] = this.id;
+//     data['title'] = this.title;
+//     data['body']=this.body;
+//     return data;
+//   }
+//
+// }
 class User{
-  int? userId;
-  int? id;
-  String? title;
-  String? body;
-
-  User({this.userId, this.id, this.title, this.body});
-
+  int? userid;
+  int  ? id;
+  String  ? title;
+  String ? body;
+  User({this.userid, this.id, required this.title,required this.body});
 
   User.fromJson(Map<String, dynamic> json) {
-    userId = json['userId'];
+    userid = json['userId'];
     id = json['id'];
     title = json['title'];
     body = json['body'];
   }
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['userId'] = this.userId;
-    data['id'] = this.id;
     data['title'] = this.title;
     data['body']=this.body;
     return data;
   }
-
 }
